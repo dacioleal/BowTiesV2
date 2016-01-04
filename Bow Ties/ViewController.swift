f6fe4de6b1638166fd4bfbeca37736c4a7fc0d89
dacioleal <dacioleal@gmail.com>
2016-01-04 20:01:59 +0100

    Initial Commit

---
+++ Bow Ties/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Bow Ties
+//
+//  Created by Dacio Leal Rodriguez on 4/1/16.
+//  Copyright © 2016 Dacio Leal Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  @IBOutlet weak var segmentedControl: UISegmentedControl!
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var ratingLabel: UILabel!
+  @IBOutlet weak var timesWornLabel: UILabel!
+  @IBOutlet weak var lastWornLabel: UILabel!
+  @IBOutlet weak var favoriteLabel: UILabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    
+  }
+  
+  @IBAction func segmentedControl(control: UISegmentedControl) {
+    
+  }
+  
+  @IBAction func wear(sender: AnyObject) {
+    
+  }
+  
+  @IBAction func rate(sender: AnyObject) {
+    
+  }
+}
+
