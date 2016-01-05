6e7895bdc02025cc20c126bc18c5be42621e5e7d
dacioleal <dacioleal@gmail.com>
2016-01-05 13:15:36 +0100

    Bowtie entity and managed object subclass created

---
+++ Bow Ties/Bowtie.swift
@@ -0,0 +1,17 @@
+//
+//  Bowtie.swift
+//  Bow Ties
+//
+//  Created by Dacio Leal Rodriguez on 5/1/16.
+//  Copyright © 2016 Dacio Leal Rodriguez. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Bowtie: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
